94cac1d0b9b23bd0175774890b23ca1cdf7dc67f
philipalassad@gmail.com <philipalassad@gmail.com>
2023-02-15 14:08:41 +0000

    Added includes for lua helper

--- LUA_SIClone/LUAHelper.h
+++ LUA_SIClone/LUAHelper.h
@@ -1,1 +1,18 @@
+/*Lua helper which file links lua with C++*/
+
 #pragma once
+
+#include <string>
+#include <functional>
+#include <map>
+
+extern "C"
+{
+	#include "../lua-5.4.4/include/lua.h"
+	#include "../lua-5.4.4/include/lauxlib.h"
+	#include "../lua-5.4.4/include/lualib.h"
+}
+
+#ifdef _WIN32
+#pragma comment(lib, "../lua-5.4.4/liblua54.a")
+#endif // _WWIN32
